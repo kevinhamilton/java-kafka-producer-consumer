1f743305cb6eb17fc20038e7e95cbf5530d2b6db
Kevin Hamilton <kevinh448@gmail.com>
2020-03-05 19:52:10 -0800

    Initial Import

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,7 +1,7 @@
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.util.Properties; 
+import java.util.Properties;
 
 public class Producer {
 
